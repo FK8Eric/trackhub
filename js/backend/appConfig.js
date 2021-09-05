9332b726437f4ad46645a812f8219030f5f02da7
Justin Sacbibit <jsacbibit@gmail.com>
2021-09-04 22:37:55 -0700

    Move config variables to .env.local

--- js/backend/appConfig.js
+++ js/backend/appConfig.js
@@ -1,10 +1,10 @@
 // TODO(flow)
-import configJson from '../../config.json';
-import secretConfigJson from '../../config.secret.json';
 
 const appConfig = {
-    ...configJson,
-    ...secretConfigJson,
+    // https://nextjs.org/docs/basic-features/environment-variables#loading-environment-variables
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '301960945914-cees61pu2lrafj8nm7e5lbgn1v23oum0.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || '',
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || '',
 };
 
 export default appConfig;
