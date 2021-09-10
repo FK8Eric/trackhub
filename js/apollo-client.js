fa2bd8a8fd6ce72c850fcdf30b93dae99a9f5309
Justin Sacbibit <jsacbibit@gmail.com>
2021-09-09 23:01:47 -0700

    Fix GraphQL API url for SSR (hopefully KEKW)

--- js/apollo-client.js
+++ js/apollo-client.js
@@ -1,8 +1,11 @@
 // @flow
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+// $FlowFixMe
+const origin = process && process.browser && window ? window.location.origin : (process && process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://trackhub.ca');
+
 const client: ApolloClient = new ApolloClient({
-    uri: `${window.location.origin}/api/graphql`,
+    uri: `${origin}/api/graphql`,
     cache: new InMemoryCache(),
 });
 
